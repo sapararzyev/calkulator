95011dceef6f073ff9a7e92807f329c8c9c1b061
sapararzyev <arzyev680@gmail.com>
2022-09-10 23:33:26 +0600

    home_workActivity2 is

--- app/src/main/java/com/example/calkulator/home_workActivity2.java
+++ app/src/main/java/com/example/calkulator/home_workActivity2.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 public class home_workActivity2 extends AppCompatActivity {
    private TextView textView;
     @Override
